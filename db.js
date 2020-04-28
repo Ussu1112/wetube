464daadc263b38f8dd941930f20f5a67822f84ce
ussu1112 <ussu1112@naver.com>
2020-04-28 18:10:49 +0900

    Wetube Chapter 5, add CSS style

--- db.js
+++ db.js
@@ -2,20 +2,18 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
-mongoose.connect(
-    process.env.MONGO_URL,
-    {
-    useNewUrlParser: true,
-    useFindAndModify:false,
-    useUnifiedTopology: true
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
 });
 
 const db = mongoose.connection;
 
-const handleOpen = () => console.log("Connect to DB")
+const handleOpen = () => console.log("Connect to DB");
 
-db.once("open",handleOpen)
+db.once("open", handleOpen);
 
-const handleError = (error) => console.log(`Error on DB Connection:${error}`)
+const handleError = (error) => console.log(`Error on DB Connection:${error}`);
 
 db.on("error", handleError);
--- middlewares.js
+++ middlewares.js
@@ -1,16 +1,16 @@
 import routes from "./router";
-import multer from "multer"
+import multer from "multer";
 
-const multerVideo = multer({ dest : "uploads/videos/" });
+const multerVideo = multer({ dest: "uploads/videos/" });
 
 export const localsMiddleware = (req, res, next) => {
-    res.locals.siteName = "WeTube";
-    res.locals.routes = routes;
-    res.locals.user = {
-        isAuthenticated: true,
-        id : 1
-    }
-    next(); //미들웨어가 next에 req를 전달해야 한다. 미들웨어가 커넥션과 라우트들 사이에 있으니 next()를 추가한다.
+  res.locals.siteName = "WeTube";
+  res.locals.routes = routes;
+  res.locals.user = {
+    isAuthenticated: false,
+    id: 1,
+  };
+  next(); // 미들웨어가 next에 req를 전달해야 한다. 미들웨어가 커넥션과 라우트들 사이에 있으니 next()를 추가한다.
 };
 
-export const uploadVideo= multerVideo.single("videoFile")
+export const uploadVideo = multerVideo.single("videoFile");
