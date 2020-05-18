57ba238f89ffec8621976d1ad62ba3597fab6b64
ussu1112 <ussu1112@naver.com>
2020-05-18 13:25:16 +0900

    Wetube 6-8 github Login

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,35 +1,79 @@
+import passport from "passport";
 import routes from "../router";
+import User from "../models/User";
 
 export const getJoin = (req, res) => {
-    res.render("join", {pageTitle : "Join"});
+  res.render("join", { pageTitle: "Join" });
 };
 
-export const postJoin = (req, res) => {
-    const{
-        body :{ name, email, password, verify_password }
-    } = req;
-    if(password != verify_password){
-        res.status(400);
-        res.render("join", {pageTitle : "Join"});
-    } else {
-        res.redirect(routes.home);
+export const postJoin = async (req, res, next) => {
+  const {
+    body: { name, email, password, verifyPassword },
+  } = req;
+  if (password !== verifyPassword) {
+    res.status(400);
+    res.render("join", { pageTitle: "Join" });
+  } else {
+    try {
+      const user = await User({
+        name,
+        email,
+      });
+      await User.register(user, password);
+      next();
+    } catch (error) {
+      console.log(error);
+      res.redirect(routes.home);
     }
-}
-
+  }
+};
 
 export const getLogin = (req, res) => {
-    res.render("login", {pageTitle : "Login"});
+  res.render("login", { pageTitle: "Login" });
 };
-export const postLogin = (req, res) => {
-    res.redirect(routes.home);
+export const postLogin = passport.authenticate("local", {
+  failureRedirect: routes.login,
+  successRedirect: routes.home,
+});
+
+export const githubLogin = passport.authenticate("github");
+
+export const githubLoginCallback = async (_, __, profile, cb) => {
+  const {
+    _json: { id, avatar_url, name, email },
+  } = profile;
+  try {
+    const user = await User.findOne({ email });
+    if (user) {
+      user.githubId = id;
+      user.save();
+      return cb(null, user);
+    }
+    const newUser = await User.create({
+      email,
+      name,
+      githubId: id,
+      avatarUrl: avatar_url,
+    });
+    return cb(null, newUser);
+  } catch (error) {
+    return cb(error);
+  }
+};
+
+export const postGithubLogIn = (req, res) => {
+  res.redirect(routes.home);
 };
 
 export const logout = (req, res) => {
-    res.redirect(routes.home);
+  req.logout();
+  res.redirect(routes.home);
 };
 
-
-export const users = (req, res) => res.render("Users", {pageTitle : "Users"});
-export const usersDetail = (req, res) => res.render("usersDetail", {pageTitle : "Users Detail"});
-export const editProfile = (req, res) => res.render("editProfile", {pageTitle : "Edit Profile"});
-export const changePassword = (req, res) => res.render("changePassword", {pageTitle : "Change Password"});
+export const users = (req, res) => res.render("Users", { pageTitle: "Users" });
+export const usersDetail = (req, res) =>
+  res.render("usersDetail", { pageTitle: "Users Detail" });
+export const editProfile = (req, res) =>
+  res.render("editProfile", { pageTitle: "Edit Profile" });
+export const changePassword = (req, res) =>
+  res.render("changePassword", { pageTitle: "Change Password" });
--- middlewares.js
+++ middlewares.js
@@ -1,16 +1,29 @@
-import routes from "./router";
 import multer from "multer";
+import routes from "./router";
 
 const multerVideo = multer({ dest: "uploads/videos/" });
 
 export const localsMiddleware = (req, res, next) => {
   res.locals.siteName = "WeTube";
   res.locals.routes = routes;
-  res.locals.user = {
-    isAuthenticated: false,
-    id: 1,
-  };
+  res.locals.user = req.user || null;
   next(); // 미들웨어가 next에 req를 전달해야 한다. 미들웨어가 커넥션과 라우트들 사이에 있으니 next()를 추가한다.
+};
+
+export const onlyPublic = (req, res, next) => {
+  if (req.user) {
+    res.redirect(routes.home);
+  } else {
+    next();
+  }
+};
+
+export const onlyPrivate = (req, res, next) => {
+  if (req.user) {
+    next();
+  } else {
+    res.redirect(routes.home);
+  }
 };
 
 export const uploadVideo = multerVideo.single("videoFile");
